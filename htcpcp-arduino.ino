5856362f924550da5f2a83d06fb3b113d09ef12e
Jorge Matricali <jorgematricali@gmail.com>
2018-09-26 10:19:43 -0300

    NTP sync

--- htcpcp-arduino.ino
+++ htcpcp-arduino.ino
@@ -17,17 +17,25 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
 
 #include "pot.h"
 #include "time.h"
 
 #define LISTEN_PORT 80
+#define LISTEN_PORT_UDP 8888
 
 /* Prototipos */
 void send_short_response(EthernetClient client, int status, String message);
 
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+const char timeServer[] = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[NTP_PACKET_SIZE];
+
 EthernetServer server(LISTEN_PORT);
+EthernetUDP Udp;
+
 pot_t *POT = NULL;
 
 void setup()
@@ -62,6 +70,54 @@ void setup()
     Serial.println("Ethernet cable is not connected.");
   }
 
+  Serial.println("Clock sync...");
+  Udp.begin(LISTEN_PORT_UDP);
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+
+  // wait to see if a reply is available
+  delay(1000);
+  if (Udp.parsePacket()) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, extract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ((epoch % 60) < 10) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+
   server.begin();
   Serial.print("Server started at: ");
   Serial.print(Ethernet.localIP());
@@ -78,7 +134,7 @@ void send_short_response(EthernetClient client, int status, String message)
   struct tm *tv;
   tv = gmtime(&timer);
   char datetime[32] = "";
-  sprintf(datetime, "Date: %02d-%02d-%04d %02d:%02d:%02d GMT", tv->tm_mday, tv->tm_mon, tv->tm_year, tv->tm_hour, tv->tm_min, tv->tm_sec);
+  sprintf(datetime, "Date: %02d-%02d-%04d %02d:%02d:%02d GMT", tv->tm_mday, tv->tm_mon, tv->tm_year + 1900, tv->tm_hour, tv->tm_min, tv->tm_sec);
   client.print("HTCPCP/1.0 ");
   client.print(status);
   client.print(" ");
@@ -94,6 +150,27 @@ void send_short_response(EthernetClient client, int status, String message)
   client.println("</h1></html>");
 }
 
+void sendNTPpacket(const char * address)
+{
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); // NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 void loop()
 {
   EthernetClient client = server.available();
