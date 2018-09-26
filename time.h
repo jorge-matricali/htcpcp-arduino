e3d6d0e00dcf392f7a40b24065343b89611f6212
Jorge Matricali <jorgematricali@gmail.com>
2018-09-26 01:09:57 -0300

    Added gmtime()

--- time.h
+++ time.h
@@ -20,8 +20,23 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 typedef unsigned long time_t;
 
+/* ISO C `broken-down time' structure.  */
+struct tm
+{
+  int tm_sec;   /* Seconds. [0-60] (1 leap second) */
+  int tm_min;   /* Minutes. [0-59] */
+  int tm_hour;  /* Hours.   [0-23] */
+  int tm_mday;  /* Day.     [1-31] */
+  int tm_mon;   /* Month.   [0-11] */
+  int tm_year;  /* Year - 1900. */
+  int tm_wday;  /* Day of week. [0-6] */
+  int tm_yday;  /* Days in year.[0-365] */
+  int tm_isdst; /* DST.   [-1/0/1]*/
+};
+
 time_t now();
 void setTime(time_t t);
 void setTime(int hr, int min, int sec, int day, int month, int yr);
+struct tm *gmtime(const time_t *timer);
 
 #endif /* TIME_H */
